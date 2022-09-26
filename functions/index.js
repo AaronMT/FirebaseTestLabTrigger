fecfec4ead47faea7e6dbd63b403845379d51ffa
Aaron Train <aaronmt@Valkyrie.localdomain>
2022-09-26 16:01:39 -0400

    Fix: Testing outcomeSummary

--- functions/index.js
+++ functions/index.js
@@ -5,23 +5,27 @@ exports.logTestComplete = functions.testLab
   .onComplete(async testMatrix => {
     const { testMatrixId, createTime, state, outcomeSummary } = testMatrix;
 
-    switch (`${outcomeSummary}`) {
+    switch (outcomeSummary) {
         case 'SUCCESS':
             functions.logger.log(
                 `Test matrix ${testMatrixId} completed successfully at ${createTime}`
             );
+            break;
         case 'FAILURE':
             functions.logger.log(
                 `Test matrix ${testMatrixId} failed at ${createTime}`
             );
+            break;
         case 'INCONCLUSIVE':
             functions.logger.log(
                 `Test matrix ${testMatrixId} inconclusive at ${createTime}`
             );
+            break;
         case 'FLAKY':
             functions.logger.log(
                 `Test matrix ${testMatrixId} flaky at ${createTime}`
             );
+            break;
         default:
             functions.logger.log(
                 `TEST ${testMatrixId} (created at ${createTime}): ${state}. ${
