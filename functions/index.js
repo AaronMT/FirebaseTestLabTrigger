92798b650e12e0bcfdbe32e1fe49e21fb8884b56
Aaron Train <aaronmt@Valkyrie.localdomain>
2022-09-26 14:17:18 -0400

    Testing outcomeSummary

--- functions/index.js
+++ functions/index.js
@@ -5,9 +5,28 @@ exports.logTestComplete = functions.testLab
   .onComplete(async testMatrix => {
     const { testMatrixId, createTime, state, outcomeSummary } = testMatrix;
 
-    functions.logger.log(
-      `TEST ${testMatrixId} (created at ${createTime}): ${state}. ${
-        outcomeSummary || ''
-      }`
-    );
+    switch (outcomeSummary) {
+       case 'success':
+            functions.logger.log(
+                `Test matrix ${testMatrixId} completed successfully at ${createTime}`
+            );
+        case 'failure':
+            functions.logger.log(
+                `Test matrix ${testMatrixId} failed at ${createTime}`
+            );
+        case 'inconclusive':
+            functions.logger.log(
+                `Test matrix ${testMatrixId} inconclusive at ${createTime}`
+            );
+        case 'flaky':
+            functions.logger.log(
+                `Test matrix ${testMatrixId} flaky at ${createTime}`
+            );
+        default:
+            functions.logger.log(
+                `TEST ${testMatrixId} (created at ${createTime}): ${state}. ${
+                  outcomeSummary || ''
+                }`
+            );
+    }
   });
